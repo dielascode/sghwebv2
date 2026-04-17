b126cd1d1afc38bae18437580526c5fd559d5b92
Aep Syaefulloh <aepsyaefulloh0@gmail.com>
2026-04-17 21:20:47 +0700

    perbaikan login

--- ec/dummyRegist.php
+++ ec/dummyRegist.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Registrasi Berhasil</title>
-    <link rel="stylesheet" href="../../style/costumer/dummyRegistStyles.css">
+    <link rel="stylesheet" href="style/costumer/dummyRegistStyles.css">
 </head>
 <body>
 
