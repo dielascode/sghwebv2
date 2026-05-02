ebb3fcf0c625b7d3a1c48304f15c4ec1436d190e
didil <faridfadilah70@gmail.com>
2026-05-02 20:38:58 +0700

    refractor: dashboard, navbar nama user yang login

--- ec/admin/index.php
+++ ec/admin/index.php
@@ -57,6 +57,7 @@ if (!isset($_SESSION['login'])) {
     };
 </script>
     <link rel="manifest" href="./assets/manifest-DTaoG9pG.json">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script type="module" crossorigin src="./assets/vendor-charts-DGwYAWel.js"></script>
     <script type="module" crossorigin src="./assets/vendor-ui-CflGdlft.js"></script>
