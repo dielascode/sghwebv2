2383ddde34afe56e6e3c2ec7b08fbfd305549feb
didil <faridfadilah70@gmail.com>
2026-05-17 19:23:36 +0700

    Fix: varietas buah tidak boleh sama

--- ec/logic/admin/varietasApi.php
+++ ec/logic/admin/varietasApi.php
@@ -25,15 +25,27 @@ class Varietas
                     b.nama_buah
                   FROM $this->table v
                   JOIN buah b ON v.id_buah = b.id";
-        
+
         return $this->conn->query($query);
     }
 
     public function store($data)
     {
+        $cek = $this->conn->prepare("SELECT * FROM $this->table WHERE id_buah = ? AND nama_varietas = ?");
+        $cek->bind_param("is", $data['id_buah'], $data['nama_varietas']);
+        $cek->execute();
+        $result = $cek->get_result();
+
+        if ($result->num_rows > 0) {
+            return [
+                'status' => false,
+                'message' => 'Varietas untuk buah ini sudah ada!'
+            ];
+        }
+
         $stmt = $this->conn->prepare("INSERT INTO $this->table (id_buah, nama_varietas) VALUES (?, ?)");
         $stmt->bind_param("is", $data['id_buah'], $data['nama_varietas']);
-        
+
         if ($stmt->execute()) {
             return ['status' => true, 'message' => 'Varietas berhasil ditambahkan!'];
         } else {
@@ -43,9 +55,21 @@ class Varietas
 
     public function update($id, $data)
     {
+        $cek = $this->conn->prepare("SELECT * FROM $this->table WHERE id_buah = ? AND nama_varietas = ? AND id != ?");
+        $cek->bind_param("isi", $data['id_buah'], $data['nama_varietas'], $id);
+        $cek->execute();
+        $result = $cek->get_result();
+
+        if ($result->num_rows > 0) {
+            return [
+                'status' => false,
+                'message' => 'Varietas sudah dipakai untuk buah ini!'
+            ];
+        }
+
         $stmt = $this->conn->prepare("UPDATE $this->table SET nama_varietas = ?, id_buah = ? WHERE id = ?");
         $stmt->bind_param("sii", $data['nama_varietas'], $data['id_buah'], $id);
-        
+
         if ($stmt->execute()) {
             return ['status' => true, 'message' => 'Varietas berhasil diupdate!'];
         } else {
@@ -57,7 +81,7 @@ class Varietas
     {
         $stmt = $this->conn->prepare("DELETE FROM $this->table WHERE id = ?");
         $stmt->bind_param("i", $id);
-        
+
         if ($stmt->execute()) {
             return ['status' => true, 'message' => 'Varietas berhasil dihapus!'];
         } else {
